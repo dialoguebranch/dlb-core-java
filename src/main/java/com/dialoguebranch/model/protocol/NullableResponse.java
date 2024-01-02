53865e85b28357c6a8cd1e7b8e227ceede413600
Harm op den Akker <hopdenakker@fruittreelabs.com>
2024-01-02 10:12:21 +0000

    Style and doc updates to NullabeResponse

--- src/main/java/com/dialoguebranch/model/protocol/NullableResponse.java
+++ src/main/java/com/dialoguebranch/model/protocol/NullableResponse.java
@@ -30,46 +30,54 @@ package com.dialoguebranch.model.protocol;
 import nl.rrd.utils.json.JsonObject;
 
 /**
- * This class is used when a web service wants to return a value or null. This
- * is to ensure that the Spring Framework returns a valid JSON string. Normally
- * if a Spring method returns null, it results in an empty response.
- * 
- * @author Dennis Hofs (RRD)
+ * This class is used when a web service wants to return a value or {@code null}. This is to ensure
+ * that the Spring Framework returns a valid JSON string. Normally if a Spring method returns
+ * {@code null}, it results in an empty response, which may throw client applications off course.
+ *
+ * @author Dennis Hofs (Roessingh Research and Development)
+ * @author Harm op den Akker (Fruit Tree Labs)
  *
  * @param <T> the value type
  */
 public class NullableResponse<T> extends JsonObject {
 	private T value = null;
 
+	// --------------------------------------------------------
+	// -------------------- Constructor(s) --------------------
+	// --------------------------------------------------------
+
 	/**
-	 * Constructs a new response with value null. This default constructor is
-	 * needed for JSON serialization.
+	 * Constructs a new response with value {@code null}. This default constructor is needed for
+	 * JSON serialization.
 	 */
-	public NullableResponse() {
-	}
+	public NullableResponse() { }
 	
 	/**
 	 * Constructs a new response with the specified value.
 	 * 
-	 * @param value the value or null
+	 * @param value the value or {@code null}
 	 */
 	public NullableResponse(T value) {
 		this.value = value;
 	}
 
+	// -----------------------------------------------------------
+	// -------------------- Getters & Setters --------------------
+	// -----------------------------------------------------------
+
 	/**
-	 * Returns the value. This can be null.
+	 * Returns the value. This can be {@code null}.
 	 * 
-	 * @return the value or null
+	 * @return the value or {@code null}
 	 */
 	public T getValue() {
 		return value;
 	}
 
 	/**
-	 * Sets the value. This can be null.
+	 * Sets the value. This can be {@code null}.
 	 * 
-	 * @param value the value or null
+	 * @param value the value or {@code null}
 	 */
 	public void setValue(T value) {
 		this.value = value;
